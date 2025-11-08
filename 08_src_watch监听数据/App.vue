f175d8ed152d73a5438792a28a81cd7b43716c60
liuping021520 <37442216@qq.com>
2025-11-08 17:05:07 +0800

    vue3中的watch函数

---
+++ 08_src_watch监听数据/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Demo></Demo>
+</template>
+
+<script>
+//引入组件
+import Demo from "./components/Demo.vue"
+export default {
+  name: "App",
+  components: { Demo },
+};
+</script>
+
+<style></style>
