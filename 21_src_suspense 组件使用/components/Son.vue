0e7b9484d1bfe21d9f37ed54e5d877026d303f8f
liuping021520 <37442216@qq.com>
2025-11-09 21:42:50 +0800

    vue3 suspense 组件使用

---
+++ 21_src_suspense 组件使用/components/Son.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="son">
+        <h2>我是Son组件</h2>
+
+        <Dialog></Dialog>
+    </div>
+</template>
+
+<script>
+import Dialog from './Dialog.vue';
+
+export default {
+    name: 'Son',
+    components: {
+        Dialog
+    }
+}
+</script>
+
+<style>
+.son {
+    background-color: blue;
+    padding: 10px;
+}
+</style>
