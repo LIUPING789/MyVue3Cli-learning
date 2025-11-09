428a76a3c7c2319fe2c00829fba6467598cf121c
liuping021520 <37442216@qq.com>
2025-11-09 15:27:53 +0800

    vue3中 shallowReactive与shallowRef使用

---
+++ 14_src_shallowReactive与shallowRef使用/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="isShowDemo = !isShowDemo"> 切换隐藏/显示</button>
+  <Demo v-if="isShowDemo"></Demo>
+</template>
+
+<script>
+//引入组件
+import { ref } from "vue";
+import Demo from "./components/Demo.vue"
+export default {
+  name: "App",
+  components: { Demo },
+
+  setup(props, context) {
+    let isShowDemo = ref(true)
+
+
+
+    return {
+      isShowDemo,
+    }
+  }
+};
+</script>
+
+<style></style>
