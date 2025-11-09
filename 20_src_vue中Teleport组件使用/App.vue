851d85eee1828694258e8bb95317514bee344c74
liuping021520 <37442216@qq.com>
2025-11-09 21:15:59 +0800

    vue3中Teleport使用

---
+++ 20_src_vue中Teleport组件使用/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <h2>我是App组件</h2>
+    <Child></Child>
+  </div>
+</template>
+
+<script>
+//引入组件
+import Child from '@/components/Child.vue';
+export default {
+  name: 'App',
+  components: {
+    Child
+  },
+}
+</script>
+
+<style>
+.app {
+  background-color: red;
+  padding: 10px;
+}
+</style>
