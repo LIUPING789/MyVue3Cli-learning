c9aa3b1691b40032b9fd804709b0962ec13152ed
liuping021520 <37442216@qq.com>
2025-11-09 21:14:55 +0800

    vue3中Teleport使用

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
   <div class="app">
-    <h2>我是App组件(祖),{{ name }}---{{ price }}</h2>
+    <h2>我是App组件</h2>
     <Child></Child>
   </div>
 </template>
 
 <script>
-//引入vue
-import { reactive, toRefs, provide, ref, isReactive, isReadonly, isRef, isProxy, readonly } from 'vue';
+//引入组件
+import Child from '@/components/Child.vue';
 export default {
   name: 'App',
-  setup(props, context) {
-    //创建数据
-    let car = reactive({
-      name: '奔驰',
-      price: 1000
-    })
-    let sum = ref(0)
-    let car2 = readonly(car)
-    // 判断响应式数据类型
-    console.log(isRef(sum))
-    console.log(isReactive(car))
-    console.log(isReadonly(car2))
-    console.log(isProxy(car))
-  }
+  components: {
+    Child
+  },
 }
 </script>
 
